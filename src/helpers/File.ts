2bfec5505d4c81017031a57e31848ba82b687781
pramudyawibowo <pramudya.wibowo72@gmail.com>
2025-06-16 17:11:22 +0700

    fix: file helper

--- src/helpers/File.ts
+++ src/helpers/File.ts
@@ -1,12 +1,11 @@
 import fs from "fs";
 import path from "path";
-import slug from "slug";
-import { v7 as uuidv7 } from "uuid";
 
 const saveFile = async (
     file: Express.Multer.File,
     directory: string,
-    maxFileSize: number = 5 * 1024 * 1024 // 5 MB
+    filename?: string,
+    maxFileSize: number = 5 * 1024 * 1024, // 5 MB
 ): Promise<string> => {
     if (file.size > maxFileSize) {
         throw new Error(`File ${file.originalname} exceeds the maximum allowed size of ${maxFileSize / (1024 * 1024)}MB.`);
@@ -15,16 +14,23 @@ const saveFile = async (
     if (directory)
         uploadDir = process.env.STORAGE_PATH ? path.join(process.env.STORAGE_PATH, directory) : path.join(__dirname, "../", uploadDir, directory);
     if (!fs.existsSync(uploadDir)) fs.mkdirSync(uploadDir, { recursive: true });
-    const filename = uuidv7() + "-" + slug(path.basename(file.originalname, path.extname(file.originalname))) + path.extname(file.originalname);
-    const uploadPath = path.join(uploadDir, filename);
+    const now = new Date();
+    const datetimeStr = `${now.getFullYear()}${String(now.getMonth() + 1).padStart(2, '0')}${String(now.getDate()).padStart(2, '0')}${String(now.getHours()).padStart(2, '0')}${String(now.getMinutes()).padStart(2, '0')}`;
+    const actualFilename = filename || datetimeStr + "_" + file.originalname;
+    const uploadPath = path.join(uploadDir, actualFilename);
     fs.writeFileSync(uploadPath, file.buffer);
     if (!fs.existsSync(uploadPath)) throw new Error("Failed to save file");
-    return directory + "/" + filename;
+    return directory + "/" + actualFilename;
 };
 
 const deleteFile = (directory: string) => {
-    const dir = process.env.STORAGE_PATH || path.join(__dirname, "../public/storage/media", directory);
+    const dir = process.env.STORAGE_PATH ? path.join(process.env.STORAGE_PATH, directory) : path.join(__dirname, "../public/storage/media", directory);
     if (fs.existsSync(dir)) fs.unlinkSync(dir);
 };
 
-export { saveFile, deleteFile };
+const getRelativePath = (directory: string) => {
+    const dir = process.env.STORAGE_PATH ? path.join(process.env.STORAGE_PATH, directory) : path.join(__dirname, "../public/storage/media", directory);
+    return dir;
+}
+
+export { saveFile, deleteFile, getRelativePath };
